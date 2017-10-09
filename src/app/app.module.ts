812705de36d5a25a6cacc972a084053bcaefd7c1
Adrian <adrianjayson13@gmail.com>
2017-10-09 20:30:14 +0800

    Adds form submission for commenting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core'; //foundation of any Angular app
 import {BrowserModule} from '@angular/platform-browser'; // package for browser specific features
 import { HttpModule } from '@angular/http'; // package for http module
+import { FormsModule } from '@angular/forms';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api'; // used for educational purposes only
 import {AppComponent} from './app.component';
-import { EntryListComponent, EntryComponent, EntryService } from './entries';
+import { EntryListComponent, EntryComponent, EntryService, EntryCommentFormComponent } from './entries';
 import { InMemoryEntryService } from './backend'; //fake backend server
 
 // decorator for modules
@@ -11,13 +12,15 @@ import { InMemoryEntryService } from './backend'; //fake backend server
     imports: [
         BrowserModule, // tells Angular the app will be used in a browser
         HttpModule,
+        FormsModule, 
         InMemoryWebApiModule.forRoot(InMemoryEntryService)
     ], 
     providers: [ EntryService ], 
     declarations: [
         AppComponent,
         EntryComponent, 
-        EntryListComponent
+        EntryListComponent,
+        EntryCommentFormComponent
     ], // registering all app's components
     bootstrap:[AppComponent] // tells w/c module will act as the base of the application
 })
